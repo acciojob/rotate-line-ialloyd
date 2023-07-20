59979db3e0c69ac00e9b6eae96d640e092a985ac
ialloyd <135848926+ialloyd@users.noreply.github.com>
2023-07-20 17:08:57 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 //your JS code here. If required.
+
+const line = document.getElementById('line');
+line.style.width = '200px';
+line.style.height = '2px';
+line.style.backgroundColor = 'black';
+let deg = 0;
+setInterval(() => {
+  line.style.transform = `rotate(${deg}deg)`;
+  deg += 1;
+}, 10);
